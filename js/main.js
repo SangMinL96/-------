c1f029765d0ac8f3f44c466328ba42d1438e987f
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-27 02:25:51 +0900

    speed leval 추가

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const items = document.querySelectorAll(".digda");
 const btn = document.querySelector("#start");
 const count = document.querySelector("#count");
+const e = document.querySelector("#eyse");
+const n = document.querySelector("#nomal");
+const h = document.querySelector("#hard");
 let currentcount = 1;
+let moveSpeed = 1500,
+  reMoveSpeed = 2000;
 const onRemove = () => {
   items.forEach((item) => {
     item.addEventListener("click", () => {
@@ -16,21 +21,33 @@ const itemNumber = () => {
   const Num = Math.floor(Math.random() * items.length);
   return Num;
 };
-console.log(itemNumber());
+
 const onMove = () => {
   const itemRandom = itemNumber();
   items[itemRandom].style.display = "block";
   const remove = () => {
     items[itemRandom].style.display = "none";
   };
-  setTimeout(remove, 2000);
+  setTimeout(remove, reMoveSpeed);
 };
 const start = () => {
   onRemove();
-  setInterval(onMove, 1500);
+  setInterval(onMove, moveSpeed);
 };
 
 const init = () => {
   btn.addEventListener("click", start);
+  e.addEventListener("click", () => {
+    moveSpeed = 2500;
+    reMoveSpeed = 3000;
+  });
+  n.addEventListener("click", () => {
+    moveSpeed = 1500;
+    reMoveSpeed = 2000;
+  });
+  h.addEventListener("click", () => {
+    moveSpeed = 500;
+    reMoveSpeed = 1000;
+  });
 };
 init();
